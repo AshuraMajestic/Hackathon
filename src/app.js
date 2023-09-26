bc3551c7030c0a6623c3368ac008f72f4b3a0485
Ashura <ashuramajestic@gmail.com>
2023-09-27 00:56:18 +0530

    hello

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5152;
 require("./db/conn.js");
 const POST = require("./model/post");
 const RegisterStudent = require("./model/student.js")
+const RegisterCollege = require("./model/college.js");
 
 const staticPath = path.join(__dirname, "../public");
 const templatePath = path.join(__dirname, "../templates/views");
@@ -68,12 +69,34 @@ app.post("/adminStudent", async (req, res) => {
 app.get("/adminUniversity", (req, res) => {
     res.render("adminUniversity");
 })
+
 app.get("/adminFaculty", (req, res) => {
     res.render("adminFaculty");
 })
 app.get("/adminCollege", (req, res) => {
     res.render("adminCollege");
 })
+app.post("/adminCollege", async (req, res) => {
+    try {
+        const registerCollege = new RegisterCollege({
+            collegename: req.body.collegename,
+            collegecode: req.body.collegecode,
+            email: req.body.email,
+            universitycode: req.body.universitycode,
+            address: req.body.address
+
+        });
+
+        const registered = await registerCollege.save();
+        res.status(201).render("adminHome");
+    } catch (error) {
+        console.log(error);
+        res.status(404).render("error", {
+            errorMessage: "Sorry, Looks like something is Wrong",
+        });
+    }
+})
+
 
 app.post("/login", async (req, res) => {
     try {
